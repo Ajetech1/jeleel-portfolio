fa4e0602a2b070425f3aa932dfdd3a8489e35a81
Ajetech1 <jeleelajetunmobi@gmail.com>
2024-05-28 22:33:11 +0100

    mobile responsiveness design

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-// function toggleNavbar() {
-//   document.querySelector(".navbar").classList.toggle("open");
-// }
+// About tablink Begins here
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Navbar side menu open & close toggler begins here
+var sideMenu = document.getElementById("sidemenu");
+
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
